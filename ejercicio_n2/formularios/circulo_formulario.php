c54d987ae789d72eb70f61fe72fad72021e4333b
Sebastorm407 <68936346+Sebastorm407@users.noreply.github.com>
2023-10-26 18:44:48 -0500

    Update circulo_formulario.php

--- ejercicio_n2/formularios/circulo_formulario.php
+++ ejercicio_n2/formularios/circulo_formulario.php
@@ -12,7 +12,7 @@
 <h1>CIRCULO</h1>
 <form action="../calcular_area.php" method="post">
 <h1>Radio</h1>
-<input style="height: 6vh;" type="number" name="radio"><br>
+<input style="height: 6vh;" type="number" name="radio" required><br>
 <div>
     <input type="hidden" name="tipo_figura" value="circulo">
 </div>
